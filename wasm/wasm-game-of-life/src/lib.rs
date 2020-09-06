d86a7cff8e521d99670c8224ab1a2cce8fd858d2
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-06 10:11:53 -0300

    Adding Interactivity.

--- wasm/wasm-game-of-life/src/lib.rs
+++ wasm/wasm-game-of-life/src/lib.rs
@@ -124,6 +124,11 @@ impl Universe {
         self.height = height;
         self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        self.cells[idx].toggle();
+    }
 }
 
 impl Universe {
@@ -175,6 +180,15 @@ impl fmt::Display for Universe {
         }
 
         Ok(())
+    }
+}
+
+impl Cell {
+    fn toggle(&mut self) {
+        *self = match *self {
+            Cell::Dead => Cell::Alive,
+            Cell::Alive => Cell::Dead,
+        };
     }
 }
 
--- wasm/wasm-game-of-life/www/index.js
+++ wasm/wasm-game-of-life/www/index.js
@@ -13,17 +13,46 @@ const height = universe.height();
 const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
+canvas.addEventListener("click", event => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / boundingRect.width;
+  const scaleY = canvas.height / boundingRect.height;
+
+  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+  universe.toggle_cell(row, col);
+
+  drawGrid();
+  drawCells();
+});
+
+const playPauseButton = document.getElementById("play-pause");
+playPauseButton.addEventListener("click", event => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
 
 const ctx = canvas.getContext('2d');
 
+let animationId = null;
+
 const renderLoop = () => {
-  debugger;
-  universe.tick();
+  //debugger;
 
   drawGrid();
   drawCells();
 
-  requestAnimationFrame(renderLoop);
+  universe.tick();
+
+  animationId = requestAnimationFrame(renderLoop);
 };
 
 const drawGrid = () => {
@@ -73,6 +102,22 @@ const drawCells = () => {
   ctx.stroke();
 };
 
-drawGrid();
-drawCells();
-requestAnimationFrame(renderLoop);
+const isPaused = () => {
+  return animationId === null;
+};
+
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  renderLoop();
+};
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  animationId = null;
+};
+
+//drawGrid();
+//drawCells();
+//requestAnimationFrame(renderLoop);
+play();

6bc671e8ba847896bad825d9ecafcd73c3f8e454
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-26 14:57:10 -0300

    1. Hello World.

--- rbe/hello_world/src/hello.rs
+++ rbe/hello_world/src/hello.rs
@@ -1,3 +1,5 @@
+// 1. Hello World
+
 // This is a comment, and is ignored by the compiler
 // You can test this code by clicking the "Run" button over there ->
 // or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
@@ -6,7 +8,7 @@
 // You can always return to the original code by clicking the "Reset" button ->
 
 // This is the main function
-fn main() {
+pub fn execute() {
     // Statements here are executed when the compiled binary is called
 
     // Print text to the console

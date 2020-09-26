dc10552196034cf7288e2aa7012a69234fa52fbf
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-26 15:23:40 -0300

    1.2.2. Display.

--- rbe/hello_world/src/main.rs
+++ rbe/hello_world/src/main.rs
@@ -2,10 +2,12 @@ mod hello;
 mod comment;
 mod print;
 mod print_debug;
+mod print_display;
 
 fn main() {
     hello::execute();
     comment::execute();
     print::execute();
     print_debug::execute();
+    print_display::execute();
 }

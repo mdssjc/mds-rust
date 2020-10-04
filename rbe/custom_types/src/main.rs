78fc16ed6ada8983cac8586f5ec09c9216216788
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-04 10:16:51 -0300

    3.2.2. C-like.

--- rbe/custom_types/src/main.rs
+++ rbe/custom_types/src/main.rs
@@ -2,10 +2,12 @@ mod custom_types;
 mod structs;
 mod ct_enum;
 mod enum_use;
+mod c_like;
 
 fn main() {
     custom_types::execute();
     structs::execute();
     ct_enum::execute();
     enum_use::execute();
+    c_like::execute();
 }

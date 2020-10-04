7529dd5c6ff21a2d429ad515158f71c8ba1500f2
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-04 10:14:02 -0300

    3.2.1. use.

--- rbe/custom_types/src/main.rs
+++ rbe/custom_types/src/main.rs
@@ -1,9 +1,11 @@
 mod custom_types;
 mod structs;
 mod ct_enum;
+mod enum_use;
 
 fn main() {
     custom_types::execute();
     structs::execute();
     ct_enum::execute();
+    enum_use::execute();
 }

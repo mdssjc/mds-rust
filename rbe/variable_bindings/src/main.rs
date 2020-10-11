25fe468aa3cb7b595c4d0702d58adf1739b53bf9
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-11 20:11:23 -0300

    4.3. Declare first.

--- rbe/variable_bindings/src/main.rs
+++ rbe/variable_bindings/src/main.rs
@@ -1,9 +1,11 @@
 mod variable_bindings;
 mod vb_mut;
 mod scope;
+mod declare;
 
 fn main() {
     variable_bindings::execute();
     vb_mut::execute();
     scope::execute();
+    declare::execute();
 }

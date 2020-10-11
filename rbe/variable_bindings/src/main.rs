d5894f791b6b116fdbcf6a362c46e34d396fb85d
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-11 20:13:43 -0300

    4.4. Freezing.

--- rbe/variable_bindings/src/main.rs
+++ rbe/variable_bindings/src/main.rs
@@ -2,10 +2,12 @@ mod variable_bindings;
 mod vb_mut;
 mod scope;
 mod declare;
+mod freeze;
 
 fn main() {
     variable_bindings::execute();
     vb_mut::execute();
     scope::execute();
     declare::execute();
+    freeze::execute();
 }

ca27d3bca7e33a6721786e3c66bc8315bd53c419
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-13 18:59:49 -0300

    8.2.1. Nesting and labels.

--- rbe/flow_control/src/main.rs
+++ rbe/flow_control/src/main.rs
@@ -1,7 +1,9 @@
 mod if_else;
 mod fc_loop;
+mod nested;
 
 fn main() {
     if_else::execute();
     fc_loop::execute();
+    nested::execute();
 }
---
+++ rbe/flow_control/src/nested.rs
@@ -0,0 +1,23 @@
+// 8.2.1. Nesting and labels
+
+#![allow(unreachable_code)]
+
+pub fn execute() {
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            // This would break only the inner loop
+            //break;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+}

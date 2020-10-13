01162b84169b62615f884b1ee891b93ee7aea9c4
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-13 19:03:15 -0300

    8.3. while.

--- rbe/flow_control/src/main.rs
+++ rbe/flow_control/src/main.rs
@@ -2,10 +2,12 @@ mod if_else;
 mod fc_loop;
 mod nested;
 mod fc_return;
+mod fc_while;
 
 fn main() {
     if_else::execute();
     fc_loop::execute();
     nested::execute();
     fc_return::execute();
+    fc_while::execute();
 }

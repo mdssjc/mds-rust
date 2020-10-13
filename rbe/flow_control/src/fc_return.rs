2e960624335be7e97059a83c4657f41c75155eaf
Marcelo dos Santos <mdssjc@gmail.com>
2020-10-13 19:01:56 -0300

    8.2.2. Returning from loops.

---
+++ rbe/flow_control/src/fc_return.rs
@@ -0,0 +1,15 @@
+// 8.2.2. Returning from loops
+
+pub fn execute() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+}
--- rbe/flow_control/src/main.rs
+++ rbe/flow_control/src/main.rs
@@ -1,9 +1,11 @@
 mod if_else;
 mod fc_loop;
 mod nested;
+mod fc_return;
 
 fn main() {
     if_else::execute();
     fc_loop::execute();
     nested::execute();
+    fc_return::execute();
 }

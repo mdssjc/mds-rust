bb69158aa47a7beb530c813687ab7441854929b1
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-20 11:58:30 -0300

    9.2.2. As input parameters.

--- rbe/functions/src/main.rs
+++ rbe/functions/src/main.rs
@@ -2,10 +2,12 @@ mod f_fn;
 mod methods;
 mod closures;
 mod capture;
+mod input_parameters;
 
 fn main() {
     f_fn::execute();
     methods::execute();
     closures::execute();
     capture::execute();
+    input_parameters::execute();
 }

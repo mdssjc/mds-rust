e54ed41e1426a46e3d43e459b1a3e0c1a7ee9794
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-21 12:18:52 -0300

    10.4. super and self.

--- rbe/modules/src/main.rs
+++ rbe/modules/src/main.rs
@@ -1,9 +1,11 @@
 mod visibility;
 mod struct_visibility;
 mod m_use;
+mod m_super;
 
 fn main() {
     visibility::execute();
     struct_visibility::execute();
     m_use::execute();
+    m_super::execute();
 }

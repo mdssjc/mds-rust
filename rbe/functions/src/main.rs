a4bad33ff17c85186a3e462c62679c23bac914f7
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-21 11:42:04 -0300

    9.3. Higher Order Functions.

--- rbe/functions/src/main.rs
+++ rbe/functions/src/main.rs
@@ -8,6 +8,7 @@ mod input_functions;
 mod output_parameters;
 mod iter_any;
 mod iter_find;
+mod hof;
 
 fn main() {
     f_fn::execute();
@@ -20,4 +21,5 @@ fn main() {
     output_parameters::execute();
     iter_any::execute();
     iter_find::execute();
+    hof::execute();
 }

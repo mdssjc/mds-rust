2d6ba7363506103a032fc0218f812b6f0b35a158
Marcelo dos Santos <mdssjc@gmail.com>
2021-01-27 12:03:53 -0300

    13.2. Crates.

--- rbe/attributes/src/main.rs
+++ rbe/attributes/src/main.rs
@@ -1,4 +1,10 @@
 // 13.1. dead_code
+// 13.2. Crates
+
+// This crate is a library
+#![crate_type = "lib"]
+// The library is named "rary"
+#![crate_name = "rary"]
 
 fn used_function() {}
 
@@ -9,6 +15,20 @@ fn unused_function() {}
 #[allow(dead_code)]
 fn noisy_unused_function() {}
 // FIXME ^ Add an attribute to suppress the warning
+
+pub fn public_function() {
+    println!("called rary's `public_function()`");
+}
+
+fn private_function() {
+    println!("called rary's `private_function()`");
+}
+
+pub fn indirect_access() {
+    print!("called rary's `indirect_access()`, that\n> ");
+
+    private_function();
+}
 
 fn main() {
     used_function();

8ad4059802005cfd487cdf57d0163b39e924acc9
Marcelo dos Santos <mdssjc@gmail.com>
2021-02-01 12:08:12 -0300

    14.4.1. Testcase: empty bounds.

--- rbe/generics/src/main.rs
+++ rbe/generics/src/main.rs
@@ -3,6 +3,7 @@ mod gen_fn;
 mod generics_impl;
 mod generics_trait;
 mod bounds;
+mod testcase_empty;
 
 fn main() {
     generics::execute();
@@ -10,4 +11,5 @@ fn main() {
     generics_impl::execute();
     generics_trait::execute();
     bounds::execute();
+    testcase_empty::execute();
 }

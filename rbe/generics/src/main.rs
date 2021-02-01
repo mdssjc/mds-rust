ab034c5a9087bfe2e321e60808d680266de21519
Marcelo dos Santos <mdssjc@gmail.com>
2021-02-01 12:00:34 -0300

    14.3. Traits.

--- rbe/generics/src/main.rs
+++ rbe/generics/src/main.rs
@@ -1,9 +1,11 @@
 mod generics;
 mod gen_fn;
 mod generics_impl;
+mod generics_trait;
 
 fn main() {
     generics::execute();
     gen_fn::execute();
     generics_impl::execute();
+    generics_trait::execute();
 }

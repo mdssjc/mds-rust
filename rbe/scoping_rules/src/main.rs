0d405cc1d663bc41ba2aa9c28f71b56d1456c38f
Marcelo dos Santos <mdssjc@gmail.com>
2021-02-03 12:17:28 -0300

    15.2.2. Partial moves.

--- rbe/scoping_rules/src/main.rs
+++ rbe/scoping_rules/src/main.rs
@@ -1,9 +1,11 @@
 mod raii;
 mod scope_move;
 mod move_mut;
+mod partial_move;
 
 fn main() {
     raii::execute();
     scope_move::execute();
     move_mut::execute();
+    partial_move::execute()
 }

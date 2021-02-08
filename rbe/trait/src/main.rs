31b9770f0a5945a1f458ec32c15524ff47353df9
Marcelo dos Santos <mdssjc@gmail.com>
2021-02-08 12:12:57 -0300

    16.4. Drop.

--- rbe/trait/src/main.rs
+++ rbe/trait/src/main.rs
@@ -2,10 +2,12 @@ mod rbe_trait;
 mod derive;
 mod trait_dyn;
 mod ops;
+mod drop;
 
 fn main() {
     rbe_trait::execute();
     derive::execute();
     trait_dyn::execute();
     ops::execute();
+    drop::execute();
 }

d9c8b922ffbe7f4dc86f6cda44f90110774ca43a
Marcelo dos Santos <mdssjc@gmail.com>
2021-02-10 12:18:29 -0300

    17.2. DRY (Don't Repeat Yourself).

--- rbe/macro_rules/src/main.rs
+++ rbe/macro_rules/src/main.rs
@@ -2,10 +2,12 @@ mod macros;
 mod designators;
 mod overload;
 mod repeat;
+mod dry;
 
 fn main() {
     macros::execute();
     designators::execute();
     overload::execute();
     repeat::execute();
+    dry::execute();
 }

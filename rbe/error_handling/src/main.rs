14e05ded9b736d06feb0c2e7a573ccdb88cde5b6
Marcelo dos Santos <mdssjc@gmail.com>
2021-02-11 12:16:14 -0300

    18.2.2. Combinators: map.

--- rbe/error_handling/src/main.rs
+++ rbe/error_handling/src/main.rs
@@ -1,9 +1,11 @@
 mod panic;
 mod option_unwrap;
 mod question_mark;
+mod map;
 
 fn main() {
     panic::execute();
     option_unwrap::execute();
     question_mark::execute();
+    map::execute();
 }

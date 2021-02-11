0b1cfabde71c9e3d571e1bae1be347861eb69cbd
Marcelo dos Santos <mdssjc@gmail.com>
2021-02-11 12:19:08 -0300

    18.2.3. Combinators: and_then.

--- rbe/error_handling/src/main.rs
+++ rbe/error_handling/src/main.rs
@@ -2,10 +2,12 @@ mod panic;
 mod option_unwrap;
 mod question_mark;
 mod map;
+mod and_then;
 
 fn main() {
     panic::execute();
     option_unwrap::execute();
     question_mark::execute();
     map::execute();
+    and_then::execute();
 }

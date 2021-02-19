c16592176c31fc4e23a11c3eef7429151b9e13d8
Marcelo dos Santos <mdssjc@gmail.com>
2021-02-19 12:11:41 -0300

    19.5. Result.

--- rbe/std_library_types/src/main.rs
+++ rbe/std_library_types/src/main.rs
@@ -2,10 +2,12 @@ mod std_box;
 mod vec;
 mod str;
 mod option;
+mod result;
 
 fn main() {
     std_box::execute();
     vec::execute();
     str::execute();
     option::execute();
+    result::execute();
 }

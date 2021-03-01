be2545cc1f27f751dc33b0a55ba923eb76fa6857
Marcelo dos Santos <mdssjc@gmail.com>
2021-03-01 12:10:07 -0300

    20.4.1. open.

--- rbe/std_misc/src/main.rs
+++ rbe/std_misc/src/main.rs
@@ -2,10 +2,12 @@ mod threads;
 mod testcase_mapreduce;
 mod channels;
 mod path;
+mod open;
 
 fn main() {
     threads::execute();
     testcase_mapreduce::execute();
     channels::execute();
     path::execute();
+    open::execute();
 }

9d543fa99e0aa5953ff3a633b8857f26af5703f4
Marcelo dos Santos <mdssjc@gmail.com>
2021-03-01 12:02:33 -0300

    20.3. Path.

--- rbe/std_misc/src/main.rs
+++ rbe/std_misc/src/main.rs
@@ -1,9 +1,11 @@
 mod threads;
 mod testcase_mapreduce;
 mod channels;
+mod path;
 
 fn main() {
     threads::execute();
     testcase_mapreduce::execute();
     channels::execute();
+    path::execute();
 }

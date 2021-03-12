6ab7dfe0aca04a4ed7399dc42bc10a01f74dc80a
Marcelo dos Santos <mdssjc@gmail.com>
2021-03-12 12:38:10 -0300

    1.3. Components and entities.

--- rust-sokoban/src/main.rs
+++ rust-sokoban/src/main.rs
@@ -1,29 +1,132 @@
 use std::path;
 
+use ggez;
 use ggez::{conf, Context, event, GameResult};
+use ggez::graphics;
+use ggez::graphics::DrawParam;
+use ggez::graphics::Image;
+use ggez::nalgebra as na;
+use specs::{
+    Builder, Component, join::Join, ReadStorage, RunNow, System, VecStorage, World, WorldExt,
+};
+
+// Components
+#[derive(Debug, Component, Clone, Copy)]
+#[storage(VecStorage)]
+pub struct Position {
+    x: u8,
+    y: u8,
+    z: u8,
+}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Renderable {
+    path: String,
+}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Wall {}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Player {}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Box {}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct BoxSpot {}
 
 // This struct will hold all our game state
 // For now there is nothing to be held, but we'll add
 // things shortly.
-struct Game {}
+struct Game {
+    world: World,
+}
 
-// This is the main event loop. ggez tells us to implement
-// two things:
-// - updating
-// - rendering
 impl event::EventHandler for Game {
     fn update(&mut self, _context: &mut Context) -> GameResult {
-        // TODO: update game logic here
         Ok(())
     }
 
-    fn draw(&mut self, _context: &mut Context) -> GameResult {
-        // TODO: update draw here
+    fn draw(&mut self, context: &mut Context) -> GameResult {
         Ok(())
     }
 }
 
+// Register components with the world
+pub fn register_components(world: &mut World) {
+    world.register::<Position>();
+    world.register::<Renderable>();
+    world.register::<Player>();
+    world.register::<Wall>();
+    world.register::<Box>();
+    world.register::<BoxSpot>();
+}
+
+// Create a wall entity
+pub fn create_wall(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/wall.png".to_string(),
+        })
+        .with(Wall {})
+        .build();
+}
+
+pub fn create_floor(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 5, ..position })
+        .with(Renderable {
+            path: "/images/floor.png".to_string(),
+        })
+        .build();
+}
+
+pub fn create_box(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/box.png".to_string(),
+        })
+        .with(Box {})
+        .build();
+}
+
+pub fn create_box_spot(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 9, ..position })
+        .with(Renderable {
+            path: "/images/box_spot.png".to_string(),
+        })
+        .with(BoxSpot {})
+        .build();
+}
+
+pub fn create_player(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/player.png".to_string(),
+        })
+        .with(Player {})
+        .build();
+}
+
 pub fn main() -> GameResult {
+    let mut world = World::new();
+    register_components(&mut world);
+
     // Create a game context and event loop
     let context_builder = ggez::ContextBuilder::new("rust_sokoban", "sokoban")
         .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
@@ -31,8 +134,9 @@ pub fn main() -> GameResult {
         .add_resource_path(path::PathBuf::from("./resources"));
 
     let (context, event_loop) = &mut context_builder.build()?;
+
     // Create the game state
-    let game = &mut Game {};
+    let game = &mut Game { world };
     // Run the main event loop
     event::run(context, event_loop, game)
 }
